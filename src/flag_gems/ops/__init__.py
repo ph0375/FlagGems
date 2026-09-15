90687ee01eda858afd0a343b0af90612f9715eec
Jingze <losercheems@gmail.com>
2026-09-15 11:54:37 +0800

    [KernelGen][Nvidia] Add linalg_solve operator with Triton kernel (#5320)
    * [KernelGen][Nvidia] Add linalg_solve operator with Triton kernel
    
    * fix: multi-backend support and more batch-size scenarios in test
    
    * fix: remove use_gems() context manager from tests
    
    ---------
    
    Co-authored-by: 张宾 <842884726@qq.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -592,6 +592,7 @@ from flag_gems.ops.linalg_matrix_sqrth import (
 from flag_gems.ops.linalg_norm import linalg_norm
 from flag_gems.ops.linalg_qr import linalg_qr, linalg_qr_out
 from flag_gems.ops.linalg_slogdet import linalg_slogdet
+from flag_gems.ops.linalg_solve import linalg_solve
 from flag_gems.ops.linalg_solve_triangular import (
     linalg_solve_triangular,
     linalg_solve_triangular_out,
@@ -1645,6 +1646,7 @@ __all__ = [
     "linalg_qr",
     "linalg_qr_out",
     "linalg_slogdet",
+    "linalg_solve",
     "linalg_solve_triangular",
     "linalg_solve_triangular_out",
     "linalg_svd",
